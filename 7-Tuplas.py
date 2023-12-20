b53f9ed84289a8c153f7c27add4dfe0e7b31132b
Andres Uribe <neluri@comfenalcoquindio.co>
2023-12-20 11:41:48 -0500

    7. Tuplas

---
+++ 7-Tuplas.py
@@ -0,0 +1,15 @@
+# Tuplas:Estructura de datos inmutable que contiene una secuencia ordenada de elementos.
+
+# Caracteristicas:
+
+# Es una secuencia ordenada de valores
+# Puede contener valores de cualquier tipo de datos
+# Puede contener valores de distintos tipos de datos
+# Cada posición en la tupla esta asociada a un entero llamado "indice"
+# Es inmutable. No puede ser modificada
+
+tupla=(1, 2, 3, 4)
+
+print (tupla)
+
+print(tupla[2])
