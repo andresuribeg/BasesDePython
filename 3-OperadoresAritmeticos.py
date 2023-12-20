0ed8e0c29bada03f3a22ff9355216d9c6e665518
Andres Uribe <neluri@comfenalcoquindio.co>
2023-12-20 11:35:25 -0500

    3. Operadores aritméticos

---
+++ 3-OperadoresAritmeticos.py
@@ -0,0 +1,44 @@
+# Operador + Operando = Expresión
+
+# Suma
+
+print(10 + 6)
+print(10 + 6.5)
+
+# Resta
+
+print(10 - 6)
+print(-10 + 6)
+print(10 - 6.5)
+
+# Multiplicación
+print(10 * 6)
+print(-10 * 6)
+print(10 * 6.5)
+
+# División: La división siempre retorna un número de coma flotante
+
+print(10 / 6)
+print(-10 / 6)
+print(10 / 6.5)
+
+
+# División entera (floor division)
+
+print(15 / 5)
+print(15 // 5)
+print(15.6 // 5.4) # Deberia dar 2.888888 pero solo toma la parte entera y el decimal lo aproxima por debajo 2.0
+
+# Potencia
+
+print(5**3)
+print(5**0)
+print(2**3)
+print(4.5**4.7)
+
+# Modulo: Retorna el residuo de la división
+
+print (5%2) # 1
+print(10%5) # 0
+
+# Orden de las operaciones PEMDAS (Paréntesis, Exponentes, Multiplicacion, División, Suma (Adition), Resta (Substrantion))
