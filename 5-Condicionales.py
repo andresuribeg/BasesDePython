31a10a501aa4a471083d2ca3b0957ceed8faeceb
Andres Uribe <neluri@comfenalcoquindio.co>
2023-12-20 11:40:02 -0500

    5. Condicionales

---
+++ 5-Condicionales.py
@@ -0,0 +1,44 @@
+# If
+
+# Sintaxis
+# if <condicion>:
+    # codigo
+# else:
+    # codigo
+temp=35
+
+if temp < 25:
+    print("Frio")
+else:
+    print("Calor")    
+
+#elif
+    
+# if <condicion1>:
+    #codigo
+# elif <condicion2>:
+    #codigo
+# else:
+    #codigo    
+temp=25.1
+
+if temp <= 0:
+    print("Muy Frio!!!!")
+elif temp < 25:
+    print("Frio")
+else:
+    print("Calor")    
+
+# NOTA: Puede existir mas de una clausula elfi pero solo una else
+
+# if <condicion1>:
+    #codigo
+# elif <condicion2>:
+    #codigo
+# elif <condicion3>:
+    #codigo
+# elif <condicion4>:
+    #codigo
+# else:
+    #codigo 
+    
